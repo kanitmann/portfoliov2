6fa0587f681702cdaa3482be9fae15e881887333
kanitmann <kanitmann90@gmail.com>
2022-07-08 01:42:18 +0530

    navbar.js component added :rocket:

---
+++ components/navbar.js
@@ -0,0 +1,123 @@
+import Logo from './logo'
+import NextLink from 'next/link'
+import {
+    Container,
+    Box,
+    Link,
+    Stack,
+    Heading,
+    Flex,
+    Menu,
+    MenuItem,
+    MenuList,
+    MenuButton,
+    IconButton,
+    useColorModeValue
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { IoLogoGithub } from 'react-icons/io5'
+
+const LinkItem = ({ href, path, target, children, ...props }) => {
+    const active = path == href
+    const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
+    return (
+        <NextLink href={href} passHref scroll={false}>
+            <Link
+                p={2}
+                bg={active ? 'grassTeal' : undefined}
+                color={active ? '#202023' : inactiveColor}
+                target={target}
+                {...props}
+            >
+                {children}
+            </Link>
+        </NextLink>
+    )
+}
+
+const NavBar = props => {
+    const { path } = props
+
+    return (
+        <Box
+            position={'fixed'}
+            as="nav"
+            w="100%"
+            bg={useColorModeValue('fffffff40', '#20202380')}
+            style={{ backdropFilter: 'blur(10px)' }}
+            zIndex={1}
+            {...props}
+        >
+            <Container
+                display={'flex'}
+                p={2}
+                maxW="container.md"
+                wrap="wrap"
+                align="center"
+                justify="space-between"
+            >
+                <Flex align="center" mr={5}>
+                    <Heading as="h1" size="lg" letterSpacing={'tight'}>
+                        <Logo />
+                    </Heading>
+                </Flex>
+                <Stack
+                    direction={{ base: 'column', md: 'row' }}
+                    display={{ base: 'none', md: 'flex' }}
+                    width={{ base: 'full', md: 'auto' }}
+                    alignItems="center"
+                    flexGrow={1}
+                    mt={{ base: 4, md: 0 }}
+                >
+                    <LinkItem href="/works" path={path}>
+                        Works
+                    </LinkItem>
+                    <LinkItem href="/Skills" path={path}>
+                        Skills
+                    </LinkItem>
+                    <LinkItem
+                        target="_blank"
+                        href="https://github.com/kanitmann/portfoliov2"
+                        path={path}
+                        display="inline-flex"
+                        alignItems="center"
+                        style={{ gap: 4 }}
+                        pl={2}
+                    >
+                        <IoLogoGithub />
+                        Source
+                    </LinkItem>
+                </Stack>
+                <Box flex={1} align={"right"}>
+                    <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+                        <Menu>
+                            <MenuButton as={IconButton} icon={<HamburgerIcon />}
+                                variant='outline'
+                                aria-label="Options"
+                            />
+                            <MenuList>
+                                <NextLink href="/" passHref>
+                                    <MenuItem as={Link}>About</MenuItem>
+                                </NextLink>
+                                <NextLink href="/works" passHref>
+                                    <MenuItem as={Link}>Works</MenuItem>
+                                </NextLink>
+                                <NextLink href="/skills" passHref>
+                                    <MenuItem as={Link}>Skills</MenuItem>
+                                </NextLink>
+                                <MenuItem
+                                    as={Link}
+                                    href="https://github.com/kanitmann/portfoliov2"
+                                >
+                                    View Source
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Box>
+                </Box>
+            </Container>
+        </Box>
+    )
+}
+
+export default NavBar;
