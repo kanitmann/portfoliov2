01c6be92c7b9e70fd2971fa6d134edae7f70608d
kanitmann <kanitmann90@gmail.com>
2022-07-08 12:54:03 +0530

    Theme toggle settings :book:

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from '../components/layouts/main'
 import theme from "../lib/theme";
 
+
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
