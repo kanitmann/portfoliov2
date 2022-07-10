3aa8dc9cf009e6abb1c89eb373b3c240472dedbc
kanitmann <kanitmann90@gmail.com>
2022-07-10 22:05:40 +0530

    bio component added

---
+++ components/layouts/bio.js
@@ -0,0 +1,20 @@
+import { Box } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+export const BioSection = styled(Box)`
+    padding-left: 3.4em;
+    text-indent: -3.4em;
+`
+
+export const BioYear = styled.span`
+    font-weight: bold;
+    margin-right: 1em;
+`
+
+export const BioMonth = styled.span`
+    font-weight: bold;
+    padding-left: 1em;
+    text-indent: -1em;
+    padding-top: 0.5em;
+    display: block;
+`
