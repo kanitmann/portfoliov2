19c390b5656e776de83ebd46775882817043125d
kanitmann <kanitmann90@gmail.com>
2022-07-23 14:52:14 +0530

    Added Scroll Restoration

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,26 @@ import theme from "../lib/theme";
 import Fonts from "../components/fonts";
 import { AnimatePresence } from "framer-motion";
 
+if (typeof window !== 'undefined') {
+    window.history.scrollRestoration = 'manual'
+}
+
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
             <Fonts />
             <Layout router={router}>
-                <AnimatePresence exitBeforeEnter initial={true}>
+                <AnimatePresence
+                    exitBeforeEnter
+                    initial={true}
+                    onExitComplete={() => {
+                        if (typeof window !== 'undefined') {
+                            window.scrollTo({ top: 0 })
+                        }
+                    }}
+                >
                     <Component {...pageProps} key={router.route} />
                 </AnimatePresence>
-
             </Layout>
         </ChakraProvider>
     )
