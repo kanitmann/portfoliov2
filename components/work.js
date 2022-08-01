20b9ff03b622c4518087152c7c4b8ff8f499abde
kanitmann <kanitmann90@gmail.com>
2022-08-01 23:41:03 +0530

    theoryjs - work page added

---
+++ components/work.js
@@ -0,0 +1,29 @@
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { Heading, Box, Image, Link, Badge } from '@chakra-ui/react'
+import NextLink from 'next/link'
+
+export const Title = ({ children }) => (
+    <Box>
+        <NextLink href="/works" passHref>
+            <Link>Work</Link>
+        </NextLink>
+
+        <span>
+            {' '}
+            <ChevronRightIcon />{' '}
+        </span>
+        <Heading display="inline-block" as="h3" fontSize={20} mb={4}>
+            {children}
+        </Heading>
+    </Box>
+)
+
+export const WorkImage = ({ src, alt }) => (
+    <Image borderRadius='lg' w='full' scr={src} alt={alt} mb={4} />
+)
+
+export const Meta = ({ children }) => (
+    <Badge colorScheme="green" mr={2}>
+        {children}
+    </Badge>
+)
---
+++ pages/works/theoryjs.js
@@ -0,0 +1,33 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => {
+    return (
+        <Layout title="TheoryJS">
+            <Container>
+                <Title>TheoryJS <Badge>2022</Badge> </Title>
+                <P>A Command Line Interface (CLI) Tool to quickly save what's on your mind.</P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                        <Link href="https://github.com/kanitmann/theoryJS">https://github.com/kanitmann/theoryJS <ExternalLinkIcon mx="1px" />  </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Platform</Meta>
+                        <span>Windows / Mac / Linux</span>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Stack</Meta>
+                        <span>NodeJs</span>
+                    </ListItem>
+                </List>
+                <WorkImage src="/images/Theoryjs.png" alt="TheoryJS" />
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work;
