4e067495cd43c32e3cc43c60d7850523ee0dbd35
kanitmann <kanitmann90@gmail.com>
2022-08-09 22:45:46 +0530

    Divider Added :sparkle:

--- pages/works/theoryjs.js
+++ pages/works/theoryjs.js
@@ -1,4 +1,4 @@
-import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem, Divider } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
@@ -9,7 +9,7 @@ const Work = () => {
         <Layout title="TheoryJS">
             <Container maxW='container.sm'>
                 <Title>TheoryJS <Badge>2022</Badge> </Title>
-                <P>A Command Line Interface (CLI) Tool to quickly save what&pos;s on your mind.</P>
+                <P>A Command Line Interface (CLI) Tool to quickly save what&apos;s on your mind.</P>
                 <List ml={4} my={4}>
                     <ListItem>
                         <Meta>Website</Meta>
@@ -24,6 +24,7 @@ const Work = () => {
                         <span>NodeJs</span>
                     </ListItem>
                 </List>
+                <Divider />
                 <WorkImage src="/images/Theoryjs.png" alt="TheoryJS" />
             </Container>
         </Layout>
